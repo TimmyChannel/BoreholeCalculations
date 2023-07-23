a5a1ce934c69875609382bef0ee6097a0567b784
TimmyChannel <timmykanal@gmail.com>
2023-07-23 17:56:40 +0300

    Woking...

---
+++ Models/Data/BoreholeRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoreholeCalculations.Models.Data
+{
+	public class BoreholeRepository : IBoreholeRepository
+	{
+		private List<IBorehole> _boreholes;
+
+		public BoreholeRepository(List<IBorehole> boreholes)
+		{
+			_boreholes = boreholes;
+		}
+
+		public void Add(IBorehole borehole)
+		{
+			_boreholes.Add(borehole);
+		}
+
+		public IBorehole GetById(int id)
+		{
+			return _boreholes.FirstOrDefault(b => b.ID == id);
+		}
+
+		public IEnumerable<IBorehole> GetAll()
+		{
+			return _boreholes.ToList();
+		}
+
+		public void Update(IBorehole borehole)
+		{
+			_boreholes[_boreholes.IndexOf(borehole)] = borehole;
+		}
+
+		public void Delete(int id)
+		{
+			_boreholes.RemoveAt(id);
+		}
+	}
+}
---
+++ Models/Data/IBoreholeRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoreholeCalculations.Models.Data
+{
+	public interface IBoreholeRepository
+	{
+		void Add(IBorehole well);
+		IBorehole GetById(int id);
+		IEnumerable<IBorehole> GetAll();
+		void Update(IBorehole well);
+		void Delete(int id);
+	}
+}
