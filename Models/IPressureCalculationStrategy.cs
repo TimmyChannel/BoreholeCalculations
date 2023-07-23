cae7e81524fdcc87fe71b5742ce86081e3a59b56
TimmyChannel <timmykanal@gmail.com>
2023-07-23 11:40:21 +0300

    Added Strategies for Pressure Calculations

---
+++ Models/IPressureCalculationStrategy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BoreholeCalculations.Models
+{
+	public interface IPressureCalculationStrategy
+	{
+		Task<double[]> CalculatePressureArray(double depth, double numSteps, double rhoSurface, CancellationToken cancellationToken);
+	}
+
+}
