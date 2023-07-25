280f6366641e3440f0c5978dba056d3b4ca8388f
TimmyChannel <timmykanal@gmail.com>
2023-07-25 11:09:19 +0300

    Some changes with DataGrid and added CalculationControl

--- ViewModels/BoreholeTableControlViewModel.cs
+++ ViewModels/BoreholeTableControlViewModel.cs
@@ -1,22 +1,29 @@
 ﻿using BoreholeCalculations.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
+using LiveChartsCore.Drawing;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace BoreholeCalculations.ViewModels
 {
 	public class BoreholeTableControlViewModel : ObservableObject
 	{
+		public ICommand SelectItems { get; private set; }
+
 		BoreholeService _service;
 		ObservableCollection<IBorehole> _boreholes;
-		public ObservableCollection<IBorehole> Boreholes => _boreholes;
-		List<IBorehole> _items = new List<IBorehole>();
+		public ObservableCollection<IBorehole> Boreholes { get => _boreholes; set => SetProperty(ref _boreholes, value); }
+		ObservableCollection<IBorehole> _items = new ObservableCollection<IBorehole>();
+		public ObservableCollection<IBorehole> SelectedBoreholes { get => _items; set => SetProperty(ref _items, value); }
 		Borehole _currentItem;
 		public Borehole CurrentItem { get => _currentItem; set => SetProperty(ref _currentItem, value); }
 		public BoreholeTableControlViewModel(BoreholeService service)
@@ -28,8 +35,21 @@ namespace BoreholeCalculations.ViewModels
 				_boreholes.Add(item);
 			}
 			PropertyChanged += BoreholeTableControlViewModel_PropertyChanged;
+			SelectItems = new RelayCommand<object>(MainTable_SelectedCellsChanged);
 		}
-
+		private void MainTable_SelectedCellsChanged(object sender)
+		{
+			SelectedBoreholes.Clear();
+			System.Collections.IList list = (System.Collections.IList)sender;
+			var collection = list.Cast<Borehole>();
+			Debug.WriteLine($"Count: {collection.Count()}\n");
+			foreach (var item in collection)
+			{
+				SelectedBoreholes.Add((Models.IBorehole)item);
+				Debug.WriteLine($"Name: {((Models.IBorehole)item).Name}");
+			}
+			WeakReferenceMessenger.Default.Send(_items.ToList());
+		}
 		private void BoreholeTableControlViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
 		{
 
@@ -37,12 +57,8 @@ namespace BoreholeCalculations.ViewModels
 			{
 				case nameof(CurrentItem):
 					if (CurrentItem == null) return;
-					if (_items.Count >= 5) _items.RemoveAt(0);
-					_items.Add(CurrentItem);
 					WeakReferenceMessenger.Default.Send(CurrentItem);
-					WeakReferenceMessenger.Default.Send(_items);
 					break;
-
 				default:
 					break;
 			}
